75d8fd59286ae201847817acaf2645ee34f819fb
MohsenArvan <f.n.raiat@gmail.com>
2024-05-05 13:08:48 +0330

    change directions auth.php to api.php for sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Route::get('/posts', [PostController::class, 'index']);
+// Route::post('/posts', [PostController::class, 'store'])->middleware('auth:sanctum');
+// Route::get('/posts/{post}', [PostController::class, 'show']);
+
+Route::prefix("posts")
+    ->controller(PostController::class)
+    ->group(function () {
+        Route::get('/', 'index');
+        Route::post('/', 'store')->middleware('auth:sanctum');
+        Route::get('/{post}', 'show');
+    });
+
+require __DIR__.'/auth.php';
