111574d4286dcda7c44c675e6abd1ada1017fd1b
MohsenArvan <f.n.raiat@gmail.com>
2024-05-05 13:09:27 +0330

    add resources for post and tag

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -14,6 +14,13 @@ class PostResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'image_url' => $this->image_url,
+            'tags' => $this->tags,
+            'created_at' => $this->created_at,
+        ];
     }
 }
